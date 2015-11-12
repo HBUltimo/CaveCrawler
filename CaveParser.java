12767f14bb43d23dc8f1cfa21a9a02248c5b83a8
Mitch Newhouse <Mitch Newhouse>
2015-11-12 09:03:18 -0600

    HW 12

--- CaveParser.java
+++ CaveParser.java
@@ -1,36 +1,105 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class CaveParser 
 {
 	private String theJSON;
+	private int currPos;
 	
-	public CaveParser(String json)
+	public CaveParser(String fileName)
 	{
-		this.theJSON = json;
+		Scanner input;
+		this.theJSON = "";
+		try 
+		{
+			input = new Scanner(new File(System.getProperty("user.dir") + "/src/" + fileName));
+			while(input.hasNextLine())
+			{
+				this.theJSON = this.theJSON + input.nextLine();
+			}
+		} 
+		catch (Exception e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
+	private boolean exists(char c)
+	{
+		for(int i = this.currPos; i < this.theJSON.length(); i++)
+		{
+			if(this.theJSON.charAt(i) == c)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private void advanceToNextChar(char c)
+	{
+		while(this.theJSON.charAt(this.currPos) != c)
+		{
+			this.currPos++;
+		}
+	}
+	
+	private void advancePastNextChar(char c)
+	{
+		this.advanceToNextChar(c);
+		this.currPos++;
+	}
+		
+	
+	
+	private JSONVariable getVariable()
+	{
+		int pos;
+		
+		//read in name
+		this.advancePastNextChar('"');
+		pos = this.currPos; //remember the pos of the beginning of the name
+		this.advanceToNextChar('"');
+		String name = this.theJSON.substring(pos, this.currPos);
+		
+		//move to the separator
+		this.advanceToNextChar(':');
+		
+		//read in value
+		this.advancePastNextChar('"');
+		pos = this.currPos; //remember the pos of the beginning of the value
+		this.advanceToNextChar('"');
+		String value = this.theJSON.substring(pos, this.currPos);
+		
+		JSONVariable theVariable = new JSONVariable(name, value);
+		return theVariable;
+	}
 	
 	public JSONObject parse()
 	{
-		for(int i = 0; i < this.theJSON.length(); i++)
+		JSONObject theObject = null;
+		this.currPos = 0;
+		while(this.currPos < this.theJSON.length())
 		{
-			if(theJSON.charAt(i) == '{')
+			this.advanceToNextChar('{');
+			theObject = new JSONObject();
+			theObject.addVariable(this.getVariable());
+			if(this.exists(','))
 			{
-				System.out.println("JSONObject begin");
+				this.advanceToNextChar(',');
+				theObject.addVariable(this.getVariable());
 			}
-
-			else if(theJSON.charAt(i) == '[')
-			{
-				System.out.println("JSONArray begin");
-			}
+			//How do we get an unlimited number of variables? (ie 50 in our case)
+			else 
+				{
+				break;
+				}
 			
-			else if(theJSON.charAt(i) == '{')
-			{
-				System.out.println("JSONObject end");
-			}
-			
-			else if(theJSON.charAt(i) == ']')
-			{
-				System.out.println("JSONArray end");
-			}
 		}
+		
+		return theObject;
 	}
 }
